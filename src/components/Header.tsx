0260de4cb68a64b9adf1000f9af2d407575e38b9
FILIPPO GABRIELE MAIOCCHI <fmaiocch@emeal.nttdata.com>
2023-11-17 18:20:47 +0100

    fix on HomePage and moved header and footer into HomePage component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -15,38 +15,38 @@ export const Header = ({
 	return (
 		<AppBar position="static" elevation={0} sx={{backgroundColor: theme.palette.background.paper}}>
 			<Box display="flex" 
-			alignItems="center" 
-			justifyContent="space-between"	
-			px={2}
-			sx={{
-				borderBottom: 1,
-				borderColor: "divider",
-				minHeight: "72px",
-			}}
-		>
-			<Box display="flex" alignItems="center">
+				alignItems="center" 
+				justifyContent="space-between"	
+				px={2}
+				sx={{
+					borderBottom: 1,
+					borderColor: "divider",
+					minHeight: "72px",
+				}}
+			>
+				<Box display="flex" alignItems="center">
 
 					<Box display="flex" alignItems="center" mr={2}>
 				 <img
-            src={bankLogo}
-						alt="Logo"
-						style={{maxHeight:"45px"}}
-					/>
+							src={bankLogo}
+							alt="Logo"
+							style={{maxHeight:"45px"}}
+						/>
+					</Box>
+					<Box display="flex" alignItems="center">
+						<Typography variant="body1" noWrap fontWeight={theme.typography.h6.fontWeight}>
+							{bankTitle}
+						</Typography>
+					</Box>
 				</Box>
 				<Box display="flex" alignItems="center">
-					<Typography variant="body1" noWrap fontWeight={theme.typography.h6.fontWeight}>
-						{bankTitle}
+					<Typography
+						variant="body1"
+						noWrap
+					>
+                     Servizi di pubblica utilità
 					</Typography>
 				</Box>
 			</Box>
-			<Box display="flex" alignItems="center">
-				<Typography
-					variant="body1"
-					noWrap
-				>
-                     Servizi di pubblica utilità
-				</Typography>
-			</Box>
-		</Box>
 		</AppBar>
 	);};
