11dfe2942bfedb085927e631380dac91aebfb337
Escarsel <eloisa.scarsella@nttdata.com>
2023-11-20 18:50:34 +0100

    Fix style + refactor

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,9 @@
-import { Box, useTheme } from "@mui/material";
+import { Box } from "@mui/material";
 import { TouchFooter } from "./FooterComponents/TouchFooter";
 import { ManualFooter } from "./FooterComponents/ManualFooter";
+import FooterBox from "./FooterComponents/FooterBox";
 
 export const Footer = () => {
-	const theme = useTheme();
 	const footerTouch: boolean = true;
 
 	const backButton = () => console.log("Bottone indietro");	
@@ -13,20 +13,13 @@ export const Footer = () => {
 			component="footer" 
 			borderTop={1} 
 			borderColor={footerTouch ? "divider" : "transparent" } 
-			style={{backgroundColor: theme.palette.background.paper}}
 		>
-			<Box
-				display="flex"
-				flexDirection="row"
-				alignItems="center"
-				maxWidth={"false"}
-				py={3}
-				mr={0}
-			>
+			<FooterBox>
 				{ footerTouch ? 
 					<TouchFooter backButton={backButton}/> : 
-					<ManualFooter backButton={backButton}/> }
-			</Box>
+					<ManualFooter backButton={backButton}/> 
+				}
+			</FooterBox>
 		</Box>
 	);
 };
