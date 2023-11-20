65b8bebd6582cbc7647d811ca2d1f34f642f948d
FILIPPO GABRIELE MAIOCCHI <fmaiocch@emeal.nttdata.com>
2023-11-20 17:17:34 +0100

    fixed static height

--- src/components/Layout/HomePage.tsx
+++ src/components/Layout/HomePage.tsx
@@ -1,4 +1,4 @@
-import { AppBar, useTheme } from "@mui/material";
+import { AppBar, Box, Button, Card, CardActions, CardContent, Grid, Typography, useTheme } from "@mui/material";
 import React from "react";
 
 import { Header } from "../Header";
@@ -16,9 +16,21 @@ export const HomePage = () => {
 				bankLogo={getCompletePathImage("icon-48x48.png")} 
 				serviceDescription="Servizi di pubblica utilità" 
 			/>
-			<div className="App">
-					HOME PAGE
-			</div>
+			<Box className="App" mt={5} ml={5} >
+				<Box>
+					<Typography  variant="h1" textAlign={"start"}> A quale servizio vuoi accedere? </Typography>
+					<Typography 
+						mt={2}
+						variant="h6" 
+						noWrap 
+						fontWeight={theme.typography.body2.fontWeight} 
+						color={"text.secondary"} 
+						textAlign={"start"}
+				 > 
+				 	Puoi effettuare pagamenti verso la PA e gestire le tue iniziative di welfare. 
+				 </Typography>
+				</Box>
+			</Box>
 			<Footer />
 		</>
 	);	
