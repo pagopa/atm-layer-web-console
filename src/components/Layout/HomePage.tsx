8f60b1fa211e061204f769c6c9a6caefe753b39b
FILIPPO GABRIELE MAIOCCHI <fmaiocch@emeal.nttdata.com>
2023-11-21 10:28:07 +0100

    added first card to home page

--- src/components/Layout/HomePage.tsx
+++ src/components/Layout/HomePage.tsx
@@ -1,6 +1,6 @@
-import { AppBar, Box, Button, Card, CardActions, CardContent, Grid, Typography, useTheme } from "@mui/material";
-import React from "react";
-
+import {Box, Button, Card, CardActions, CardContent, Grid, IconButton, Typography, useTheme } from "@mui/material";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import { LogoPagoPAProduct } from "@pagopa/mui-italia/dist/assets/LogoPagoPAProduct";
 import { Header } from "../Header";
 import { getCompletePathImage } from "../../utils/Commons";
 import { Footer } from "../Footer";
@@ -16,12 +16,12 @@ export const HomePage = () => {
 				bankLogo={getCompletePathImage("icon-48x48.png")} 
 				serviceDescription="Servizi di pubblica utilità" 
 			/>
-			<Box className="App" mt={5} ml={5} >
+			<Box className="App" mt={3} ml={3} >
 				<Box>
-					<Typography  variant="h1" textAlign={"start"}> A quale servizio vuoi accedere? </Typography>
+					<Typography  variant="h5" textAlign={"start"}> A quale servizio vuoi accedere? </Typography>
 					<Typography 
-						mt={2}
-						variant="h6" 
+						mt={1}
+						variant="subtitle2" 
 						noWrap 
 						fontWeight={theme.typography.body2.fontWeight} 
 						color={"text.secondary"} 
@@ -29,6 +29,36 @@ export const HomePage = () => {
 				 > 
 				 	Puoi effettuare pagamenti verso la PA e gestire le tue iniziative di welfare. 
 				 </Typography>
+				</Box>
+				<Box mt={2}>
+					<Grid container my={2} xs={3.5} padding={"8px"}>
+						<Card
+							style={{ 
+								width: "100%", 
+								border: theme.cardStyle?.border,
+								borderColor: "lightgrey",
+								borderRadius: "8px",
+							}}>
+							<CardContent sx={{ pt: "16px", pb: "8px", px: "16px"}}>
+								<Typography variant="h6" gutterBottom>
+        						Paga un avviso pagoPA
+								</Typography>
+								
+							</CardContent>
+							<CardActions sx={{ pt: "0px", pb: "16px", px: "16px"}}>
+								<Grid item xs={6} alignContent="center">
+									<Box textAlign="start" sx={{ pt: "12px" }}>
+										<LogoPagoPAProduct color="default" title={""} size={32} />
+									</Box>
+								</Grid>
+								<Grid item xs={6} alignContent="center">
+									<Box textAlign="end"><IconButton size="small"> 
+										<ArrowForwardIosIcon fontSize="small" color="primary" />
+									</IconButton> </Box>
+								</Grid>
+							</CardActions>
+						</Card>
+					</Grid>
 				</Box>
 			</Box>
 			<Footer />
