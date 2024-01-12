a9643543baf8c61db27f40ad8a8418b6b59058df
LuciaM1 <lucia.mazzocchi@nttdata.com>
2024-01-12 10:34:09 +0100

    modifying homepage layout

--- src/pages/Layout/HomePage3.tsx
+++ src/pages/Layout/HomePage3.tsx
@@ -1,26 +1,41 @@
 import React from "react";
-import { Grid } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import HomeCardComponent from "../../components/CardComponents/HomeCardComponent";
 import { homePageCardItems } from "../../utils/HomePageCardItems";
+import { TitleComponent } from "../../components/TitleComponents/TitleComponent";
+import { Header } from "../../components/Header";
+import { HomePageTitle } from "../../components/TitleComponents/HomePageTitle";
 
 
-function HomePge3(){
+function HomePage3(){
 	return (
  
 		<>
-			<Grid container item xs={12} spacing={12} p={12}>
-				{
-					homePageCardItems.map((e, i) => (
-						<HomeCardComponent
-							key={i}
-							title={e.title}
-							description={e.description}
-							pageLink={e.pageLink}
-						/>
-					)
-					)
-				}
-			</Grid></>
+			<Box>
+				<Header bankLogo={""} bankTitle={"titolo"} serviceDescription={"descrizione"} />
+			</Box>
+
+			<Box marginTop={10} textAlign={"center"}>
+				<HomePageTitle 
+					title={"Console management"} 
+					subTitle={"Console per la gestione delle risorse"}
+				/>
+			</Box>
+			<Box marginTop={10}>
+				<Grid container item xs={12} spacing={12} p={12} alignItems={"center"} justifyContent={"center"}>
+					{
+						homePageCardItems.map((e, i) => (
+							<HomeCardComponent
+								key={i}
+								title={e.title}
+								description={e.description}
+								pageLink={e.pageLink}
+							/>
+						)
+						)
+					}
+				</Grid>
+			</Box></>
 	);};
 
-export default HomePge3;    
+export default HomePage3;    
