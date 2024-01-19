dfe10aa9ec465f9d4c74696cd668ec3502bfa458
Escarsel <eloisa.scarsella@nttdata.com>
2024-01-19 12:20:27 +0100

    Fix routing

--- src/App.tsx
+++ src/App.tsx
@@ -15,17 +15,22 @@ import NoPage from "./pages/NoPage";
 
 const LocalRoutes = () => (
 	
-	<Routes>
-		<Route path="/" element={<PageLayout page={<Layout />} />} />
-		<Route index element={<PageLayout page={<HomePage />} />} />
-		<Route path={routes.BPMN} element={<PageLayout page={<BpmnPage />} />} />
-		<Route path={routes.WARNING_CODE} element={<PageLayout page={<WarningCodeInput />} />} />
-		<Route
-			path={routes.ERROR_PAGE}
-			element={<PageLayout page={<CommonErrorPage title={""} icon={undefined} />} />}
-		/>
-		 <Route path="*" element={<NoPage />} />
-	</Routes>
+	<BrowserRouter basename="/web-console">
+		<PageLayout>
+			<Routes>
+				<Route path="/" element={<Layout />} />
+				<Route index path={routes.HOME} element={<Layout children={<HomePage />} />} />
+				<Route path={routes.BPMN} element={<Layout children={<BpmnPage />} />} />
+				<Route path={routes.WARNING_CODE} element={<Layout children={<WarningCodeInput />} />} />
+				<Route
+					path={routes.ERROR_PAGE}
+					element={<Layout children={<CommonErrorPage title={""} icon={undefined} />} />}
+				/>
+				<Route path="*" element={<NoPage />} />
+			</Routes>
+		</PageLayout>
+	</BrowserRouter>
+
 	
 );
 
@@ -52,9 +57,7 @@ function App() {
 	return (
 		<ThemeProvider theme={themeApp}>
 			<Ctx.Provider value={values}>
-				<BrowserRouter>
-					{LocalRoutes()}
-				</BrowserRouter>
+				{LocalRoutes()}
 			</Ctx.Provider>
 		</ThemeProvider>
 	);
