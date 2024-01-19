f34da11ccf8d57061bae82b6e61a7678988a8dde
Escarsel <eloisa.scarsella@nttdata.com>
2024-01-19 12:50:31 +0100

    Fix dev

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,4 @@
-export const BASE_ROUTE = "http://localhost:3005";
+export const BASE_ROUTE = "http://pagopa-dev-atm-layer-frontend-486872590.eu-south-1.elb.amazonaws.com";
 
 const ROUTES = {
 	HOME: "home",
