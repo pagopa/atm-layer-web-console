555b5c39fe8f5f7ec2966190e1449e7ec45899ef
Escarsel <eloisa.scarsella@nttdata.com>
2024-01-22 10:25:55 +0100

    Fix desc layout

--- src/utils/HomePageCardItems.ts
+++ src/utils/HomePageCardItems.ts
@@ -9,21 +9,21 @@ export const homePageCardItems = [
 		pageLink: routes.HOME
 	},
 	{
-		title: "Processi",
+		title: "Risorse di processo",
 		id: "process",
-		description:"I processi sono file BPMN che, una volta deployati, istruiscono gli step seguiti dal terminale ATM.",
+		description:"I processi sono file BPMN che, una volta deployati, costituiranno gli step del flusso da seguire sul terminale ATM.",
 		icon:"AccountTree",
 		pageLink: routes.BPMN
 	},
 	{
 		title: "Risorse statiche",
 		id: "static",
-		description:"Le risorse statiche sono file che possono essere visualizzati nel terminale ATM (HTML, immagini,...)  o più in generale file che possono essere [completare]",
+		description:"Le risorse statiche sono file HTML, immagini che possono essere visualizzati nel terminale ATM per la costruzione della pagina.",
 		icon:"Description",
 		pageLink: "/"
 	},
 	{
-		title: "Risorse per processi",
+		title: "Risorse aggiuntive per processi",
 		id: "workflow",
 		description:"File BPMN, DMN o FORM possono essere deployati e inclusi in più di un processo contemporaneamente, a supporto del porcesso stesso.",
 		icon:"Widgets",
