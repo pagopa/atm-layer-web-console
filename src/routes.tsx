167a5e92d434a6e7bbf37a5cf4a200cbb5f0f4c1
Escarsel <eloisa.scarsella@nttdata.com>
2024-01-22 09:28:27 +0100

    corretta build

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,4 @@
-export const BASE_ROUTE = "http://pagopa-dev-atm-layer-frontend-486872590.eu-south-1.elb.amazonaws.com";
+// export const BASE_ROUTE = "http://pagopa-dev-atm-layer-frontend-486872590.eu-south-1.elb.amazonaws.com";
 
 const ROUTES = {
 	HOME: "home",
