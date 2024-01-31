9c9890d37354e8b098f0fcc00682aa6c868ac178
Escarsel <80228890+asioleLab@users.noreply.github.com>
2024-01-31 17:14:28 +0100

    Fix (#31)

--- src/hook/fetch/fetchGetAllFiltered.ts
+++ src/hook/fetch/fetchGetAllFiltered.ts
@@ -1,7 +1,7 @@
 import fetch from "../useFetch";
 
 const fetchGetAllFiltered = ({ abortController, url }: any) => async () => {
-	const fetchFromServer = fetch(process.env.REACT_APP_BACKEND_URL);
+	const fetchFromServer = fetch();
 
 	const data = await fetchFromServer({
 		urlEndpoint: url,
