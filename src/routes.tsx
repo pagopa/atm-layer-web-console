788c6ed4ed02176e3f26d0577e59deb8138b6da6
Escarsel <80228890+asioleLab@users.noreply.github.com>
2024-02-05 11:30:28 +0100

    Aggiunte rotte login (#36)
    lgtm

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,4 +1,3 @@
-// export const BASE_ROUTE = "http://pagopa-dev-atm-layer-frontend-486872590.eu-south-1.elb.amazonaws.com";
 
 const ROUTES = {
 	HOME: "/home",
@@ -8,14 +7,9 @@ const ROUTES = {
 	RESOURCES: "/resources",
 	WORKFLOW_RESOURCES: "/workflow_resources",
 	BPMN_DETAILS: "/bpmnId/:bpmnId/modelVersion/:modelVersion",
-	CREATE_BPMN: "/bpmn/create"
+	CREATE_BPMN: "/bpmn/create",
+	LOGIN:"/login",
+	LOGIN_BACK:"/login/callback"
 };
-// const ROUTES = {
-// 	HOME: "home",
-// 	WARNING_CODE: "warning-code",
-// 	ERROR_PAGE: "error",
-// 	BPMN: "bpmn",
-// 	RESOURCES: "resources",
-// 	WORKFLOW_RESOURCES: "workflow_resources",
 
 export default ROUTES;
