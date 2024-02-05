045aaf75197ecc961e85e33289a3d231514213d4
Escarsel <eloisa.scarsella@nttdata.com>
2024-02-05 11:56:40 +0100

    Aggiunte rotte login

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,5 +1,3 @@
-// export const BASE_ROUTE = "http://pagopa-dev-atm-layer-frontend-486872590.eu-south-1.elb.amazonaws.com";
-
 const ROUTES = {
 	HOME: "/home",
 	WARNING_CODE: "/warning-code",
@@ -9,14 +7,9 @@ const ROUTES = {
 	WORKFLOW_RESOURCES: "/workflow_resources",
 	BPMN_DETAILS: "/bpmnId/:bpmnId/modelVersion/:modelVersion",
 	CREATE_BPMN: "/bpmn/create",
-	CREATE_WORKFLOW_RESOURCES: "/workflow_resources/create"
+	CREATE_WORKFLOW_RESOURCES: "/workflow_resources/create",
+	LOGIN: "/login",
+	LOGIN_BACK:"/login/callback"
 };
-// const ROUTES = {
-// 	HOME: "home",
-// 	WARNING_CODE: "warning-code",
-// 	ERROR_PAGE: "error",
-// 	BPMN: "bpmn",
-// 	RESOURCES: "resources",
-// 	WORKFLOW_RESOURCES: "workflow_resources",
 
 export default ROUTES;
