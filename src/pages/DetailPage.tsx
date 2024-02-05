59702ace3252151c0ca2aa8d4f6aa3f8450521c2
ElisKina-dev <elis.kina@nttdata.com>
2024-02-05 12:48:42 +0100

    detail buttons added

--- src/pages/DetailPage.tsx
+++ src/pages/DetailPage.tsx
@@ -1,10 +1,12 @@
+import ActionsBox from "../components/Commons/ActionsBox";
 import DetailBox from "../components/Commons/DetailBox";
 import BoxPageLayout from "./Layout/BoxPageLayout";
 
 
 const DetailPage = () => (
 	<BoxPageLayout px={10}>
-		<DetailBox></DetailBox>
+		<DetailBox />
+		<ActionsBox />
 	</BoxPageLayout>
 );
 
