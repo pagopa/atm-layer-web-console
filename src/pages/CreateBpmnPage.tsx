a0ec3926b7ca2544b254460002de06c196915da2
USERSAD\evalenti <emanuele.valentini@nttdata.com>
2024-02-06 17:33:23 +0100

    Breadcrumb refactor for generalization

--- src/pages/CreateBpmnPage.tsx
+++ src/pages/CreateBpmnPage.tsx
@@ -1,26 +1,17 @@
 import { Box, Typography } from "@mui/material";
 import CreateBpmn from "../components/FormComponents/FormsBpmn/CreateBpmn";
-import BreadCrumb from "../components/NavigationComponents/Breadcrumb";
+import BreadCrumb from "../components/NavigationComponents/BreadcrumbComponent";
+import BreadCrumbMapper from "../components/NavigationComponents/BreadCrumbMapper";
 import FormPageTemplate from "./Layout/FormPageTemplate";
 
 
 const CreateBpmnPage = () => {
-	const breadComponent = [
-		<Typography key="1" color="text.primary">
-			 Home
-		</Typography>,
-		<Typography key="2" color="text.primary">
-			Risorse di processo
-		</Typography>,
-		<Typography key="3" color="primary">
-            Creazione risorsa di processo
-		</Typography>
-	];
+	const breadComponent = ["Home", "Risorse di processo", "Creazione risorsa di processo"];
 
 	return(
 		<>
 			<Box mb={2} display={"flex"} justifyContent={"flex-start"} alignItems={"center"} mt={5} ml={10}>
-				<BreadCrumb breadcrumb={breadComponent} mb={"4px"} />
+				<BreadCrumb breadcrumb={BreadCrumbMapper(breadComponent)} mb={"4px"} />
 			</Box><FormPageTemplate>
 				<CreateBpmn />
 			</FormPageTemplate>
