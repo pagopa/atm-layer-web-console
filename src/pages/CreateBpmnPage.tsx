42aca34a0b3365dda1f9a07a703c097bc07c6f9f
ElisKina-dev <elis.kina@nttdata.com>
2024-02-06 16:15:24 +0100

    breadcrumbs for createbpmn & upgrade

--- src/pages/CreateBpmnPage.tsx
+++ src/pages/CreateBpmnPage.tsx
@@ -1,11 +1,31 @@
+import { Box, Typography } from "@mui/material";
 import CreateBpmn from "../components/FormComponents/FormsBpmn/CreateBpmn";
+import BreadCrumb from "../components/NavigationComponents/Breadcrumb";
 import FormPageTemplate from "./Layout/FormPageTemplate";
 
-const CreateBpmnPage = () => (
-	<FormPageTemplate>
-		<CreateBpmn />
-	</FormPageTemplate>
 
-);
+const CreateBpmnPage = () => {
+	const breadComponent = [
+		<Typography key="1" color="text.primary">
+			 Home
+		</Typography>,
+		<Typography key="2" color="text.primary">
+			Risorse di processo
+		</Typography>,
+		<Typography key="3" color="primary">
+            Creazione risorsa di processo
+		</Typography>
+	];
+
+	return(
+		<>
+			<Box mb={2} display={"flex"} justifyContent={"flex-start"} alignItems={"center"} mt={5} ml={10}>
+				<BreadCrumb breadcrumb={breadComponent} mb={"4px"} />
+			</Box><FormPageTemplate>
+				<CreateBpmn />
+			</FormPageTemplate>
+		</>
+	);
+};
 
 export default CreateBpmnPage;
