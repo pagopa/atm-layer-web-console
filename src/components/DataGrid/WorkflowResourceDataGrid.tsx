2205859ffeb321d9125423ac7059ab5d3ebe7aa2
Gabriele Maiocchi <fmaiocch@emeal.nttdata.com>
2024-02-09 16:09:40 +0100

    Refactor WorkFLowResourcePage and WorkFLowResourceDataGrid

--- src/components/DataGrid/WorkflowResourceDataGrid.tsx
+++ src/components/DataGrid/WorkflowResourceDataGrid.tsx
@@ -1,37 +1,70 @@
 import { GridColDef, GridColumnVisibilityModel } from "@mui/x-data-grid";
 import { Box } from "@mui/material";
-import { useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
+import { Ctx } from "../../DataContext";
+import { WORKFLOW_RESOURCE } from "../../commons/constants";
+import { GET_ALL_WORKFLOW_RESOURCES_FILTER } from "../../commons/endpoints";
+import fetchGetAllWfResourcesFiltered from "../../hook/fetch/WorkflowResource/fetchGetAllWfResourcesFiltered";
+import { getQueryString } from "../../utils/Commons";
 import { CustomDataGrid } from "./CustomDataGrid";
+import FilterBar from "./FilterBar";
+import TableColumn from "./TableColumn";
 
-type Props = {
-	tableList: any;
-	columns: Array<GridColDef<any>>;
-	columnVisibilityModel: GridColumnVisibilityModel;
-	filterValues?: any;
-	getAllList: (filterValues?: any, pageIndex?: any) => void;
-	setPaginationModel: React.Dispatch<React.SetStateAction<{
-		page: number;
-		pageSize: number;
-	}>>;
-	paginationModel: {
-		page: number;
-		pageSize: number;
+export const WorkflowResourceDataGrid = () => {
+
+	const initialValues = {
+		resourceType: "",
+		filename: "",
+		status: ""
 	};
-	totalItemsFound: number;
-};
-
-export const WorkflowResourceDataGrid = ({ tableList, columns, columnVisibilityModel, filterValues, getAllList, setPaginationModel, paginationModel, totalItemsFound }: Props) => {
-
+	const { abortController } = useContext(Ctx);
+	const [tableListWfResources, setTableListWfResources] = useState<any>([]);
+	const [filterValues, setFilterValues] = useState(initialValues);
+	const [paginationModel, setPaginationModel] = useState({
+		page: 0,
+		pageSize: 10,
+	});
+	const { buildColumnDefs, visibleColumns } = TableColumn();
+	const columns: Array<GridColDef> = buildColumnDefs(WORKFLOW_RESOURCE);
+	const [totalItemsFound, setTotalItemsFound] = useState(0);
 	const rowHeight = 55;
+
+	const getAllWfResourcesList = async (filterValues?: any, pageIndex?: number): Promise<void> => {
+		const URL = `${GET_ALL_WORKFLOW_RESOURCES_FILTER}?pageIndex=${pageIndex ?? paginationModel.page}&pageSize=${paginationModel.pageSize}`;
+		const url = getQueryString(URL, filterValues, WORKFLOW_RESOURCE);
+
+		try {
+			const response = await fetchGetAllWfResourcesFiltered({ abortController, url })();
+			if (response?.success) {
+				const { page, limit, results, itemsFound } = response.valuesObj;
+				setTableListWfResources(results);
+				setPaginationModel({ page, pageSize: limit });
+				setTotalItemsFound(itemsFound);
+			} else {
+				setTableListWfResources([]);
+			}
+		} catch (error) {
+			console.error("ERROR", error);
+		}
+	};
+	
 
 	useEffect(() => {
 		if (!Object.values(filterValues).some(value => value !== "")) {
-			getAllList();
+			void getAllWfResourcesList();
 		}
 	}, []);
 
 	return (
 		<Box p={2}>
+			<FilterBar
+				filterValues={filterValues}
+				setFilterValues={setFilterValues}
+				setTableList={tableListWfResources}
+				getAllList={getAllWfResourcesList}
+				newFilterValues={initialValues}
+				driver={WORKFLOW_RESOURCE}
+			/>
 			<CustomDataGrid
 				disableColumnFilter
 				disableColumnSelector
@@ -44,15 +77,15 @@ export const WorkflowResourceDataGrid = ({ tableList, columns, columnVisibilityM
 				getRowId={(r) => r.workflowResourceId.concat(r.createdAt)}
 				hideFooterSelectedRowCount={true}
 				rowHeight={rowHeight}
-				rows={tableList}
+				rows={tableListWfResources}
 				rowCount={totalItemsFound}
 				sortingMode="server"
-				columnVisibilityModel={{ ...columnVisibilityModel }}
+				columnVisibilityModel={visibleColumns(WORKFLOW_RESOURCE)}
 				paginationMode="server"
 				pagination
 				pageSizeOptions={[10]}
 				paginationModel={{ ...paginationModel }}
-				onPaginationModelChange={(newPage) => getAllList(filterValues, newPage.page)}
+				onPaginationModelChange={(newPage) => getAllWfResourcesList(filterValues, newPage.page)}
 			/>
 		</Box>
 	);
--- src/pages/WorkflowResourcePage.tsx
+++ src/pages/WorkflowResourcePage.tsx
@@ -1,76 +1,10 @@
-import { GridColDef } from "@mui/x-data-grid";
-import { useContext, useState } from "react";
-import { Ctx } from "../DataContext";
-import { WORKFLOW_RESOURCE } from "../commons/constants";
-import { GET_ALL_WORKFLOW_RESOURCES_FILTER } from "../commons/endpoints";
-import FilterBar from "../components/DataGrid/FilterBar";
-import TableColumn from "../components/DataGrid/TableColumn";
-import fetchGetAllWfResourcesFiltered from "../hook/fetch/WorkflowResource/fetchGetAllWfResourcesFiltered";
-import { getQueryString } from "../utils/Commons";
 import WorkflowResourceDataGrid from "../components/DataGrid/WorkflowResourceDataGrid";
 import BoxPageLayout from "./Layout/BoxPageLayout";
 
-const WorkflowResourcePage = () => {
-	const initialValues = {
-		resourceType: "",
-		filename: "",
-		status: ""
-	};
-
-	const { abortController } = useContext(Ctx);
-	const [tableListWfResources, setTableListWfResources] = useState<any>([]);
-	const [filterValues, setFilterValues] = useState(initialValues);
-	const [paginationModel, setPaginationModel] = useState({
-		page: 0,
-		pageSize: 10,
-	});
-	const { buildColumnDefs, visibleColumns } = TableColumn();
-	const columns: Array<GridColDef> = buildColumnDefs(WORKFLOW_RESOURCE);
-	const [totalItemsFound, setTotalItemsFound] = useState(0);
-
-
-	const getAllWfResourcesList = async (filterValues?: any, pageIndex?: number): Promise<void> => {
-		const URL = `${GET_ALL_WORKFLOW_RESOURCES_FILTER}?pageIndex=${pageIndex ?? paginationModel.page}&pageSize=${paginationModel.pageSize}`;
-		const url = getQueryString(URL, filterValues, WORKFLOW_RESOURCE);
-
-		try {
-			const response = await fetchGetAllWfResourcesFiltered({ abortController, url })();
-			if (response?.success) {
-				const { page, limit, results, itemsFound } = response.valuesObj;
-				setTableListWfResources(results);
-				setPaginationModel({ page, pageSize: limit });
-				setTotalItemsFound(itemsFound);
-			} else {
-				setTableListWfResources([]);
-			}
-		} catch (error) {
-			console.error("ERROR", error);
-		}
-	};
-
-	return (
-		<BoxPageLayout shadow={true} px={0} mx={5}>
-			<FilterBar
-				filterValues={filterValues}
-				setFilterValues={setFilterValues}
-				setTableList={tableListWfResources}
-				getAllList={getAllWfResourcesList}
-				newFilterValues={initialValues}
-				driver={WORKFLOW_RESOURCE}
-			/>
-			<WorkflowResourceDataGrid
-				tableList={tableListWfResources}
-				columns={columns}
-				columnVisibilityModel={visibleColumns(WORKFLOW_RESOURCE)}
-				filterValues={filterValues}
-				getAllList={getAllWfResourcesList}
-				setPaginationModel={setPaginationModel}
-				paginationModel={paginationModel}
-				totalItemsFound={totalItemsFound}
-			/>
-		</BoxPageLayout>
-	);
-};
-
+const WorkflowResourcePage = () => (
+	<BoxPageLayout shadow={true} px={0} mx={5}>
+		<WorkflowResourceDataGrid />
+	</BoxPageLayout>
+);
 
 export default WorkflowResourcePage;
