1c3327583d63287ce56ef1c7d89cda895704e357
Gabriele Maiocchi <fmaiocch@emeal.nttdata.com>
2024-02-14 15:47:19 +0100

    fix regExp pattern type string

--- src/utils/checks.ts
+++ src/utils/checks.ts
@@ -114,7 +114,7 @@ const checks = () => {
 	// };
 
 	const isValidUUID = (uuid: string) => {
-		const uuidRegex = new RegExp(/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/);
+		const uuidRegex = new RegExp("/^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/");
 		return uuidRegex.test(uuid);
 	};
 
