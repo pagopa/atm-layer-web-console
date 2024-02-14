fc316c600615c062b36f079da1d655d2c5f8d431
Escarsel <eloisa.scarsella@nttdata.com>
2024-02-14 19:23:56 +0100

    Fix breadcrumb

--- src/components/NavigationComponents/CommonDetailBreadCrumb.tsx
+++ src/components/NavigationComponents/CommonDetailBreadCrumb.tsx
@@ -2,7 +2,7 @@ import { generatePath } from "react-router";
 import Link from "@mui/material/Link";
 import ROUTES from "../../routes";
 
-export const breadCrumbLinkComponent = (message: string) => {
+export const breadCrumbLinkComponent = (currentPage: string) => {
 	const recordParams = JSON.parse(localStorage.getItem("recordParams") ?? "");
 	const hrefValue = generatePath(`/webconsole/${ROUTES.BPMN_DETAILS}`, { bpmnId: recordParams.bpmnId, modelVersion: recordParams.modelVersion });
 	
@@ -13,9 +13,10 @@ export const breadCrumbLinkComponent = (message: string) => {
 			key="link"
 			href={hrefValue}
 			color="inherit"
+			underline="hover"
 		>
             Dettaglio risorsa di processo
 		</Link>,
-		message
+		currentPage
 	];
 };
