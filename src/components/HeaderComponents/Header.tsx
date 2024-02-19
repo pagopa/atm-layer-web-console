7bdb15cf947e776c9b06d735c58d8886d4c83663
Escarsel <80228890+asioleLab@users.noreply.github.com>
2024-02-19 12:05:10 +0100

    Fix datagrid (#51)

--- src/components/HeaderComponents/Header.tsx
+++ src/components/HeaderComponents/Header.tsx
@@ -11,12 +11,8 @@ export const Header = () => {
 	
 	const handleLogin=()=>{
 		const urlLogin=process.env.REACT_APP_LOGIN_URL;
-		const newWindow = window.open(urlLogin, "_blank", "noopener,noreferrer");
-		if (newWindow) {
-			console.log("url login", urlLogin);
-			// eslint-disable-next-line functional/immutable-data
-			newWindow.opener = null;
-		}
+		console.log("url login", urlLogin);
+		window.open(urlLogin, "_blank");
 	};
 	
 	const handleLogout=()=>{
