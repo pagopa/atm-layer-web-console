c75db2379ab3878ada7b9cdfd343446dd6b7fb9e
LuciaM1 <lucia.mazzocchi@nttdata.com>
2024-03-01 11:26:17 +0100

    adding UploadFileComponents tests

---
+++ src/components/UploadFileComponents/__tests__/UploadFileWithButton.test.tsx
@@ -0,0 +1,69 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import UploadFileWithButton from '../UploadFileWithButton';
+import { toBeInTheDocument } from '@testing-library/jest-dom/matchers';
+import { useTheme } from '@mui/material';
+
+describe("UploadFileWithButton", () => {
+    
+    const mockOnChange = jest.fn();
+    const mockOnClick = jest.fn();
+    const testFile = new File(["hello"], "hello.png", { type: "image/png" })
+
+
+    test("Clear Uploaded File", () => {
+        render(<UploadFileWithButton 
+            name="Test Input Name"
+            allowedType="image/png"
+            file= {testFile}
+            onChange= {mockOnChange}
+            onClick= {mockOnClick} />);
+        
+        const clearButton = screen.getByTestId("clear-upload-button");
+        expect(clearButton).toBeInTheDocument();
+        fireEvent.click(clearButton);
+        
+        expect(mockOnClick).toHaveBeenCalled();
+      });
+
+    test("Upload Allowed File", () =>{
+        render(<UploadFileWithButton 
+            name="Test Input Name"
+            allowedType="image/png"
+            onChange= {mockOnChange}
+            onClick= {mockOnClick} />);
+
+        const clearButton = screen.queryByTestId("clear-upload-button");
+        expect(clearButton).not.toBeInTheDocument();
+        const inputFile = screen.getByTestId("hidden-input");
+        expect(inputFile).toBeInTheDocument();
+        fireEvent.change(inputFile);
+        expect(mockOnChange).toHaveBeenCalled();
+    });
+
+    test("Uploaded File With Error", () => {
+        render(<UploadFileWithButton 
+            name ="Test Input Name"
+            allowedType ="audio/*"
+            file={testFile}
+            onChange = {mockOnChange}
+            onClick = {mockOnClick}
+            error = "Test Error Message" />);
+
+            const text = screen.getByRole('button');
+            expect(text).toBeInTheDocument();
+    });
+
+    test("No File Uploaded With Error", () =>{
+        render(<UploadFileWithButton 
+            name="Test Input Name"
+            allowedType="image/png"
+            onChange= {mockOnChange}
+            onClick= {mockOnClick}
+            error = "Test Error Message" />);
+
+        const clearButton = screen.queryByTestId("clear-upload-button");
+        expect(clearButton).not.toBeInTheDocument();
+        const inputFile = screen.getByTestId("hidden-input");
+        expect(inputFile).toBeInTheDocument();
+    });
+})
