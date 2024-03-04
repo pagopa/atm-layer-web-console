62efaea78eaa3061a6e6c5f19fd0c3f693639809
Escarsel <eloisa.scarsella@nttdata.com>
2024-03-04 19:53:21 +0100

    Completato test LoginCallBack + esclusa cartella model

--- src/pages/__tests__/LoginPageCallback.test.tsx
+++ src/pages/__tests__/LoginPageCallback.test.tsx
@@ -1,7 +1,18 @@
-import { render, act } from "@testing-library/react";
+import { render, act, waitFor } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { Ctx } from "../../DataContext";
 import LoginPageCallback from "../LoginPageCallback";
+
+const originalFetch = global.fetch;
+
+beforeEach(() => {
+  jest.spyOn(console, 'error').mockImplementation(() => { });
+  jest.spyOn(console, 'warn').mockImplementation(() => { });
+});
+
+afterEach(() => {
+  global.fetch = originalFetch;
+})
 
 
 describe("LoginPageCallback", () => {
@@ -39,5 +50,32 @@ describe("LoginPageCallback", () => {
     });
 
     expect(window.location.pathname).toBe("/login");
+  });
+
+  test("Get username from valid token", async () => {
+    const setLogged = jest.fn();
+    global.fetch = jest.fn().mockResolvedValueOnce({
+        json: () => Promise.resolve({
+            status: 200,
+            success: true,
+            valuesObj: {
+              email:"giuseppe.demaio@pagopa.it"
+            },
+        }),
+    });
+
+    act(() => {
+      render(
+        <Router>
+          <Ctx.Provider value={{ setLogged }}>
+            <LoginPageCallback />
+          </Ctx.Provider>
+        </Router>
+      );
+    });
+
+    await waitFor(() => {
+        expect(global.fetch).toHaveBeenCalled();
+    });
   });
 });
