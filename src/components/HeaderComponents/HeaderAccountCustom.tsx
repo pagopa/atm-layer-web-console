577e44e1e221c35341c9afc3a638bfed1c65cdd1
LuciaM1 <144435187+LuciaM1@users.noreply.github.com>
2024-03-13 11:08:37 +0100

    fixes (#87)
    Co-authored-by: Escarsel <eloisa.scarsella@nttdata.com>

--- src/components/HeaderComponents/HeaderAccountCustom.tsx
+++ src/components/HeaderComponents/HeaderAccountCustom.tsx
@@ -1,10 +1,12 @@
 import { Container, Button, Stack, Typography } from "@mui/material";
 import AccountCircleRoundedIcon from "@mui/icons-material/AccountCircleRounded";
 import { Box } from "@mui/system";
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { RootLinkType } from "../../model/UserModel";
 import { Ctx } from "../../DataContext";
-import EmulatorButton from "../NavigationComponents/EmulatorButton";
+// import EmulatorButton from "../NavigationComponents/EmulatorButton";
+import { fetchRequest } from "../../hook/fetch/fetchRequest";
+import { USER_EMAIL } from "../../commons/endpoints";
 
 type HeaderAccountProps = {
     rootLink: RootLinkType;
@@ -18,7 +20,27 @@ export const HeaderAccountCustom = ({
 	onLogout
 }: HeaderAccountProps) => {
 
-	const { userEmail } = useContext(Ctx);
+	const { userEmail, setUserEmail, abortController } = useContext(Ctx);
+
+	const token = localStorage.getItem("jwt_console");
+
+	const getTokenEmail = async () => {
+		try {
+			const response = await fetchRequest({ urlEndpoint: USER_EMAIL, method: "GET", abortController })();
+
+			if (response?.success) {
+				setUserEmail({ email: response?.valuesObj.email });
+			}
+		} catch (error) {
+			console.error("ERROR", error);
+		}
+	};
+
+	useEffect(() => {
+		if(!userEmail.email && token){
+			void getTokenEmail();
+		}
+	}, []);
 		
 	return (
 		<Stack
--- src/pages/LoginPageCallback.tsx
+++ src/pages/LoginPageCallback.tsx
@@ -3,8 +3,6 @@ import { useNavigate } from "react-router-dom";
 import { Ctx } from "../DataContext";
 import routes from "../routes";
 import { Loading } from "../components/Commons/Loading";
-import { fetchRequest } from "../hook/fetch/fetchRequest";
-import { USER_EMAIL } from "../commons/endpoints";
 
 
 const LoginPageCallback = () => {
@@ -16,26 +14,12 @@ const LoginPageCallback = () => {
 		if (token) {
 			setLogged(true);
 			localStorage.setItem("jwt_console", token);
-			void getTokenEmail();
 			navigate(routes.HOME);
 		} else {
 			navigate(routes.LOGIN);
 		}
 	}, []);
 
-	const getTokenEmail = async () => {
-		try {
-			const response = await fetchRequest({ urlEndpoint: USER_EMAIL, method: "GET", abortController })();
-
-			if (response?.success) {
-				setUserEmail({ email: response?.valuesObj.email });
-			} else {
-				setUserEmail("Benvenuto utente");
-			}
-		} catch (error) {
-			console.error("ERROR", error);
-		}
-	};
 
 	return (
 		<Loading />
