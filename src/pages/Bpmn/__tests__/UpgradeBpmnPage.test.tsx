91704beb8c97c289a4d541637318a1da3cb20b47
EmVal <emanuele.valentini@nttdata.com>
2024-06-20 14:53:16 +0200

    Increase Coverage

--- src/pages/Bpmn/__tests__/UpgradeBpmnPage.test.tsx
+++ src/pages/Bpmn/__tests__/UpgradeBpmnPage.test.tsx
@@ -10,11 +10,16 @@ beforeEach(() => {
 });
 
 describe("UpgradeBpmnPage", () => {
-    sessionStorage.setItem("recordParams", JSON.stringify(bpmnTableMocked.results[0]))
-
     const abortController = new AbortController();
 
     test("Test UpgradeBpmnPage", () => {
+        jest.spyOn(Storage.prototype, 'getItem').mockImplementation((key) => {
+            if (key === 'recordParams') {
+                return JSON.stringify(bpmnTableMocked.results[0]);
+            }
+            return null;
+        });
+
         render(
             <Ctx.Provider value={{ abortController }}>
                 <BrowserRouter>
@@ -22,6 +27,27 @@ describe("UpgradeBpmnPage", () => {
                 </BrowserRouter>
             </Ctx.Provider>
         );
+
         expect(screen.getAllByText("Aggiornamento risorsa di processo")[0]).toBeInTheDocument();
-    })
+    });
+
+    test("populates recordParams correctly when recordParamsString is present in sessionStorage", () => {
+        const mockRecordParams = bpmnTableMocked.results[0];
+        jest.spyOn(Storage.prototype, 'getItem').mockImplementation((key) => {
+            if (key === 'recordParams') {
+                return JSON.stringify(mockRecordParams);
+            }
+            return null;
+        });
+
+        render(
+            <Ctx.Provider value={{ abortController }}>
+                <BrowserRouter>
+                    <UpgradeBpmnPage />
+                </BrowserRouter>
+            </Ctx.Provider>
+        );
+
+        expect(sessionStorage.getItem).toHaveBeenCalledWith("recordParams");
+    });
 });
