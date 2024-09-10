d8419d22d88c3515c88c4149385e7962740a4453
LuciaM1 <lucia.mazzocchi@nttdata.com>
2024-09-10 11:32:11 +0200

    fixing openapi

--- src/components/Commons/Commons.tsx
+++ src/components/Commons/Commons.tsx
@@ -90,7 +90,7 @@ export const getQueryString = (filterValues: any, driver: string, URL?: string):
 		appendQueryParam("name", filterValues?.name);
 		appendQueryParam("surname", filterValues?.surname);
 		appendQueryParam("userId", filterValues?.userId);
-		appendQueryParam("profileId", filterValues?.profileId);
+		appendQueryParam("profileNumber", filterValues?.profileId);
 		break;
 	default:
 		return "";
